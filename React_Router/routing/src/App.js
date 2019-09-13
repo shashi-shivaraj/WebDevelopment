e6fe145b0b518913a6502459b7c6e32a61ba38db
shashi-shivaraju <shashi2shivaraj@gmail.com>
2019-09-13 00:23:27 -0500

    initial commit

---
+++ React_Router/routing/src/App.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "./App.css";
+import Nav from "./nav";
+import About from "./about";
+import Shop from "./shop";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Nav />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/about" exact component={About} />
+          <Route path="/shop" exact component={Shop} />
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+const Home = () => (
+  <div>
+    <h1>Home Page</h1>
+  </div>
+);
+
+export default App;
---
+++ React_Router/routing/src/nav.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./App.css";
+import { Link } from "react-router-dom";
+
+function Nav() {
+  const navstyle = {
+    color: "white"
+  };
+  return (
+    <nav>
+      <h3>Logo</h3>
+      <ul className="nav-links">
+        <Link style={navstyle} to="/about">
+          <li>About</li>
+        </Link>
+        <Link style={navstyle} to="/shop">
+          <li>Shop</li>
+        </Link>
+      </ul>
+    </nav>
+  );
+}
+
+export default Nav;
